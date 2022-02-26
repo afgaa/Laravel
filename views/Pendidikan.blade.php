0067377beb0146c0c2bc64bb02fb54aa399aad53
afgaa <95727836+afgaa@users.noreply.github.com>
2022-02-26 11:01:17 +0700

    Add files via upload

---
+++ views/Pendidikan.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <div class="center">
+            <title>Riwayat Pendidikan</title>
+            <link rel="stylesheet" href="css/css.css"></link>
+        </div>
+    </head>
+
+    <body>
+        <header>
+            <div>
+                <center>
+                <h1><b>Riwayat Pendidikan</h1></b>
+                <h2>Primo Afga Parlevi</h2>
+                <p>Selama 20 tahun</p>
+                </center>
+            </div>
+            <div>
+                <nav>
+                    <ul>
+                        <a href="/Profile">Biodata</a>
+                        <a href="/Pendidikan">Riwayat Pendidikan</a> 
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    <br>
+    <center>
+    <h1 align="center">Riwayat Pendidikan</h1>
+    <br>
+    <table border="1" cellspacing="0" cellpadding="5" align="center" width="800">
+        <tr align="center" bgcolor=#a9a9a9>
+            <td width="50">No</td>
+            <td width="300">Tahun</td>
+            <td width="300">Tempat Belajar</td>
+        </tr>
+            <tr>
+                <th>1.</th><th>2006-2008</th><th>TK PGRI 02 Tirtoyudho</th>
+            </tr>
+            <tr>
+                <th>2.</th><th>2008-2014</th><th>SDN 01 Sumbertangkil</th>
+            </tr>
+            <tr>
+                <th>3.</th><th>2014-2017</th><th>SMP PGRI 04 Tirtoyudho</th>
+            </tr>
+            <tr>
+                <th>4.</th><th>2017-2020</th><th>SMKN4 Malang</th>
+            </tr>
+            <tr>
+                <th>5.</th><th>2020-Sekarang</th><th>Politeknik Negeri Malang</th>
+            </tr>
+    </table>
+    </center>
+    <br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br>
+    <br><br>
+    <footer class="center">
+            <p>@Copyright Afga</p>
+        </footer>
+    </body>    
+</html>
---
+++ views/Profile.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>Profile</title>
+        <link rel="stylesheet" href="css/css.css"></link>
+    </head>
+
+    <body>
+        <header>
+            <div>
+                <a class="center"><h1>Bio Saya</h1></a>
+            </div>
+            <div>
+                <nav>
+                    <ul>
+                        <a href="/Profile">Biodata</a>
+                        <a href="/Pendidikan">Riwayat Pendidikan</a> 
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <br>
+        <center> 
+            <a><img src="img/12.jpeg" width="300" height="400"></a>
+            <br><br>
+            <div>
+              <p>Primo Afga Parlevi<br>
+                  2041720021<br>
+                  Mahasiswa Semester 4 Berusia 20 Tahun<br>
+                  D4 Teknik Informatika<br>
+                  Teknologi Informasi<br>
+                  Politeknik Negeri Malang<br>
+                  Asal Malang</p>
+          </div>
+        </center>
+        <br><br>
+        <footer class="center">
+            <p>@Copyright Afga</p>
+        </footer>
+    </body>
+</html>
